deeaddbe5ffebff95fa7cbd04350b28856a8db52
DJ VENIX <yashyfactz@gmail.com>
2026-04-16 15:40:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/audio/:id", async (req, res) => {
+    const assetId = req.params.id;
+
+    try {
+        const product = await axios.get(
+            `https://apis.roblox.com/marketplace-items/v1/items/details`,
+            { params: { itemIds: assetId } }
+        );
+
+        const item = product.data[0];
+        if (!item) return res.json({ success: false });
+
+        const thumbRes = await axios.get(
+            `https://thumbnails.roblox.com/v1/assets`,
+            {
+                params: {
+                    assetIds: assetId,
+                    size: "420x420",
+                    format: "Png"
+                }
+            }
+        );
+
+        const thumbData = thumbRes.data.data[0];
+
+        res.json({
+            success: true,
+            creatorType: item.creatorType,
+            creatorId: item.creatorTargetId,
+            hasThumbnail: thumbData && thumbData.imageUrl !== null,
+            thumbnailUrl: thumbData?.imageUrl || null
+        });
+
+    } catch (err) {
+        res.json({ success: false });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server running");
+});
